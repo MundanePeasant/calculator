366180977f23585846559202029963bacdec56f7
Mundane <mundanepeasant@gmail.com>
2022-04-06 19:41:34 -0500

    Adding functions, clear, and enter capabilities

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //variables used throughout
 var firstNum = '0';
 var secNum = '0';
+var operator = '';
 
 
 //event listenters
@@ -21,9 +22,99 @@ for(var i = 0; i < 10; i++){
 
 document.getElementById("clear").addEventListener("click", clear);
 
+document.getElementById("enter").addEventListener("click", functionSelector);
+
+document.getElementById("add").addEventListener("click", ()=> {
+    functionSelector();
+    secNum = firstNum;
+    firstNum = '0';
+    operator = 'add';
+});
+
+document.getElementById("subtract").addEventListener("click", ()=> {
+    functionSelector();
+    secNum = firstNum;
+    firstNum = '0';
+    operator = 'subtract';
+});
+
+document.getElementById("multiply").addEventListener("click", ()=> {
+    functionSelector();
+    secNum = firstNum;
+    firstNum = '0';
+    operator = 'multiply';
+});
+
+document.getElementById("divide").addEventListener("click", ()=> {
+    functionSelector();
+    secNum = firstNum;
+    firstNum = '0';
+    operator = 'divide';
+});
+
 
 //functions
 function clear(){
     firstNum = '0';
+    updateDisplay();
+}
+
+function updateDisplay(){
     document.getElementsByClassName("display")[0].innerHTML = firstNum;
+}
+
+function functionSelector(){
+    if(operator == 'add'){
+        add(secNum, firstNum);
+    }
+    else if(operator == 'subtract'){
+        subtract(secNum, firstNum);
+    }
+    else if(operator == 'multiply'){
+        multiply(secNum, firstNum);
+    }
+    else if(operator == 'divide'){
+        divide(secNum, firstNum);
+    }
+}
+
+function add(first, second){
+    first = parseFloat(first);
+    second = parseFloat(second);
+    var fun = Math.round((first + second)*10.0) / 10.0;
+    firstNum = fun.toString();
+    secNum = '0';
+
+    updateDisplay();
+}
+
+function subtract(first, second){
+    //subtracts the two values
+    first = parseFloat(first);
+    second = parseFloat(second);
+    var fun = Math.round((first - second)*10.0) / 10.0;
+    parseFloat(first);
+    firstNum = fun.toString();
+    secNum = '0';
+
+    updateDisplay();
+}
+
+function multiply(first, second){
+    //adds the two values
+
+    var fun = Math.round((parseFloat(first) * parseFloat(second)*10)) / 10;
+    firstNum = fun.toString();
+    secNum = '0';
+
+    updateDisplay();
+}
+
+function divide(first, second){
+    //adds the two values
+    var fun = Math.round((parseFloat(first) / parseFloat(second)*10)) / 10;
+    firstNum = fun.toString();
+    secNum = '0';
+
+    updateDisplay();
 }
